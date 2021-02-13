2219a70bb036c25d145d981f4004dac32e3d1d16
Shana Hoehn <shanahoehn@gmail.com>
2021-02-13 15:16:23 -0800

    add app

---
+++ client/App.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import './style.css';
+
+class App extends Component{
+    constructor(props) {
+      super(props);
+
+      this.state = {};
+    }
+
+    componentDidMount() {
+        console.log("inside component did mount")
+        fetch('/api')
+          // .then(console.log('did mount'))
+          .then(res => res.json())//on client .json is parsing 
+          .then((data) => {
+          })
+          .catch(err => console.log('get project: ERROR: ', err));
+        }
+    
+render()
+
+
+
+};
