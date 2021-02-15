60fc0467e1746440bd7ad272a84188f4c86ca36c
Shana Hoehn <shanahoehn@gmail.com>
2021-02-15 14:32:14 -0800

    add donation form

--- client/App.jsx
+++ client/App.jsx
@@ -12,7 +12,11 @@ class App extends Component{
         fetch('/getDonations')
           .then(res => res.json())
           .then((totals) => {
-
+              const totalRaised = totals;
+              return this.setState({
+                  ...this.state,
+                  totalRaised : totals
+              })
           })
           .catch(err => console.log('get project: ERROR: ', err));
         }
@@ -24,7 +28,7 @@ render() {
             <div className="main">
                 <h1>Codesmith Alumni Scholarship</h1>
                 <p>info about scholarship info about scholarship info about scholarship info about scholarship info about scholarship info about scholarship</p>   
-                <h3 id="totalHomePage">Total Raised ${total}</h3>
+                <h3 id="totalHomePage">Total Raised ${this.state.totalRaised}</h3>
             </div>
                 <div className="btn">
                     <button id="donateHome"> Donate </button>
