7693e5a3b8e98020c7fce2cff03731d45ba1eb02
Vicki Lee <leevicki01@gmail.com>
2021-02-17 14:28:46 -0800

    add mainstage component

--- client/display/Homepage.jsx
+++ client/display/Homepage.jsx
@@ -7,17 +7,32 @@ import TotalDonations from '../components/home/TotalDonations.jsx';
 class Homepage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      amount: null,
+      firstName: null,
+      lastName: null,
+    };
+    this.mainstageForm = this.mainstageForm.bind(this);
   }
 
   componentDidMount() {
   }
 
+  mainstageForm(amountInput, firstInput, lastInput) {
+    console.log('mainstageForm: amountInput', amountInput);
+    this.setState({
+      amount: amountInput,
+      firstName: firstInput,
+      lastName: lastInput,
+    });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div>
         <Navbar />
-        <Mainstage />
+        <Mainstage mainstageForm={this.mainstageForm} />
         <div>
           <Info />
           <TotalDonations />
