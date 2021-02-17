13246b330ffd75759ed52e9b700ccd714e21a209
Adrian Inza-Cruz <ainzacruz@gmail.com>
2021-02-16 20:17:23 -0800

    frontend Boiler Plate

---
+++ client/components/home/TotalDonations.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const TotalDonations = () => <div>hello!</div>;
+
+export default TotalDonations;
---
+++ client/display/Homepage.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Navbar from '../components/home/Navbar';
+import Mainstage from '../components/home/Mainstage';
+import Info from '../components/home/Info';
+import TotalDonations from '../components/home/TotalDonations';
+
+class Homepage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Mainstage />
+        <div>
+          <Info />
+          <TotalDonations />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
