d240b94e9326aab18106c685ee021c6aab9656ee
Jenny Hai <jennyhai@jennys-mbp.lan>
2021-02-18 12:45:42 -0800

    add account details route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,27 +1,27 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-const dataBaseController = require('../controllers/donationsController.js');
+const donationsController = require('../controllers/donationsController.js');
 const userController = require('../controllers/usersController');
 const shoutoutsController = require('../controllers/shoutoutsController');
 
-router.get('/donations', dataBaseController.getDonationList, (req, res) => {
+router.get('/donations', donationsController.getDonationList, (req, res) => {
   res.status(200).json(res.locals.donationsList);
 });
 
-router.get('/donations/total', dataBaseController.getDonationTotal, (req, res) => {
+router.get('/donations/total', donationsController.getDonationTotal, (req, res) => {
   res.status(200).json(res.locals.donationsTotal);
 });
 
-router.post('/donations', dataBaseController.createDonation, userController.createUser, (req, res) => {
+router.post('/donations', donationsController.createDonation, userController.createUser, userController.setSSIDCookie, userController.linkUser, (req, res) => {
   res.status(200).json(res.locals.inputDonation);
 });
 
-router.post('/signup', userController.createUser, (req, res) => {
+router.post('/signup', userController.createUser, userController.setSSIDCookie, (req, res) => {
   res.status(200).json(res.locals.user);
 });
 
-router.post('/login', userController.login, (req, res) => {
+router.post('/login', userController.login, userController.setSSIDCookie, (req, res) => {
   res.status(200).send('You are logged in');
 });
 
@@ -35,6 +35,10 @@ router.post('/shoutouts', shoutoutsController.createShoutout, (req, res) => {
 
 router.post('/incrementclaps', shoutoutsController.incrementClaps, (req, res) => {
   res.status(200).json(res.locals.claps);
+});
+
+router.get('/accountDetails/:userID', userController.accountDetails, (req, res)=>{
+  res.status(200).json(res.locals.accountDetails);
 });
 
 module.exports = router;
