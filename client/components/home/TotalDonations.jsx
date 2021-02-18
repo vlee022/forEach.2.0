37f74eb940b81a381cbd3b883785a7ea56fd6123
Adrian Inza-Cruz <ainzacruz@gmail.com>
2021-02-17 16:05:02 -0800

    total donations in progress

--- client/components/home/TotalDonations.jsx
+++ client/components/home/TotalDonations.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
-const TotalDonations = () => <div>hello!</div>;
+const TotalDonations = (props) => (
+  <div>
+    <h2 className="total-donations">
+      $100
+    </h2>
+    <div className="bar">---</div>
+    <div className="donors">
+      <p>
+        <span>10</span>
+        <span>donors</span>
+      </p>
+    </div>
+    <div>
+      <div>
+        <img alt="testing" src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Y29kaW5nfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+        <div>
+          <h3>Annonymous</h3>
+          <p>$5</p>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default TotalDonations;
--- client/display/Homepage.jsx
+++ client/display/Homepage.jsx
@@ -5,6 +5,7 @@ import TotalDonations from '../components/home/TotalDonations.jsx';
 import Payments from '../components/payments/Payments.jsx';
 import Login from '../components/login/Login.jsx';
 import Navbar from '../components/home/Navbar.jsx';
+import Shoutouts from '../components/shoutouts/Shoutouts.jsx';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 class Homepage extends Component {
@@ -24,9 +25,11 @@ class Homepage extends Component {
           <Switch>
             <Route path="/" exact>
               <Mainstage />
+              <TotalDonations />
             </Route>
-            <Route path="/donate" exact component={Payments} />
-            <Route path="/login" exact component={Login} />
+            <Route path="/donate" component={Payments} />
+            <Route path="/login" component={Login} />
+            <Route path="/shoutouts" component={Shoutouts} />
           </Switch>
         </Router>
       </div>
